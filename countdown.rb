b7b692eaeb8c3a3730f4931e3e122f82825e8d3d
Matoya Williams <matoyawilliams@yahoo.com>
2020-06-07 00:47:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,7 +7,7 @@ end
 "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep(seconds)
+def countdown_with_sleep(seconds_left)
 until seconds_left <1
   puts "#{seconds_left} SECOND(S)!"
   seconds_left -=1 
